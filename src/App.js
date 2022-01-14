89a6b5244f6ded692514a9abd2874324dcaf50ba
AtsushiNi <atsushi9731@gmail.com>
2022-01-14 17:34:56 +0900

    react-pro-sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react'
-import './App.css';
 import { Authenticator } from '@aws-amplify/ui-react'
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar'
-import { FaHeart, FaGem} from 'react-icons/fa'
-import 'react-pro-sidebar/dist/css/styles.css'
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent } from 'react-pro-sidebar'
+import { FaHeart, FaUser, FaUserCheck, FaRegSun } from 'react-icons/fa'
+import { MdGroup } from 'react-icons/md'
 import '@aws-amplify/ui-react/styles.css'
+import './styles/App.scss'
 
 function App() {
   return (
     <div className="App">
-      <Authenticator valiation='modal'>
-        <ProSidebar>
-          <Menu iconShape='square'>
-            <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-            <SubMenu title='components' icon={<FaHeart />}>
-              <MenuItem>Component 1</MenuItem>
-              <MenuItem>Component 2</MenuItem>
-            </SubMenu>
-          </Menu>
-        </ProSidebar>
+      <Authenticator variation='modal'>
+        {({signOut, user }) => (
+          <div>
+            <ProSidebar>
+              <SidebarHeader>
+                <div
+                  style={{
+                    padding: '24px',
+                    textTransform: 'uppercase',
+                    fontWeight: 'bold',
+                    fontSize: 14,
+                    letterSpacing: '1px',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                  }}
+                >
+                  Attendance App
+                </div>
+              </SidebarHeader>
+              <SidebarContent>
+                <Menu iconShape='square'>
+                  <SubMenu title='マイページ' icon={<FaHeart />}>
+                    <MenuItem>打刻</MenuItem>
+                    <MenuItem>出勤簿</MenuItem>
+                    <MenuItem>申請</MenuItem>
+                    <MenuItem>メンバー</MenuItem>
+                    <MenuItem>所属部署</MenuItem>
+                  </SubMenu>
+                  <MenuItem icon={<FaUserCheck />}>承認</MenuItem>
+                  <MenuItem icon={<MdGroup />}>部署</MenuItem>
+                  <MenuItem icon={<FaUser />}>社員</MenuItem>
+                </Menu>
+                <Menu>
+                  <MenuItem icon={<FaRegSun />}>設定</MenuItem>
+                </Menu>
+              </SidebarContent>
+            </ProSidebar>
+            <button onClick={signOut}>sign out</button>
+          </div>
+        )}
       </Authenticator>
     </div>
   );
