f07a350d2ade11fb72fdd47b164a32af1c546298
AtsushiNi <atsushi9731@gmail.com>
2022-01-21 12:45:31 +0900

    div調整

--- src/App.js
+++ src/App.js
@@ -40,79 +40,77 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Authenticator variation='modal'>
-        {({signOut, user }) => (
-          <div>
-            <ProSidebar>
-              <SidebarHeader>
-                <div
-                  style={{
-                    padding: '24px',
-                    textTransform: 'uppercase',
-                    fontWeight: 'bold',
-                    fontSize: 14,
-                    letterSpacing: '1px',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    whiteSpace: 'nowrap',
-                  }}
-                >
-                  Attendance App
-                </div>
-              </SidebarHeader>
-              <SidebarContent>
-                <Menu iconShape='square'>
-                  <SubMenu title='マイページ' icon={<FaHeart />}>
-                    <MenuItem>打刻</MenuItem>
-                    <MenuItem>出勤簿</MenuItem>
-                    <MenuItem>申請</MenuItem>
-                    <MenuItem>メンバー</MenuItem>
-                    <MenuItem>所属部署</MenuItem>
-                  </SubMenu>
-                  <MenuItem icon={<FaUserCheck />} style={getStyle('application')} id='application'>
-                    承認
-                    <Link to='/applications' />
-                  </MenuItem>
-                  <MenuItem icon={<MdGroup />} style={getStyle('groups')} id='groups'>
-                    部署
-                    <Link to='/groups' />
-                  </MenuItem>
-                  <MenuItem icon={<FaUser />} style={getStyle('users')} id='users'>
-                    社員
-                    <Link to='/users' />
-                  </MenuItem>
-                </Menu>
-                <Menu>
-                  <MenuItem icon={<FaRegSun />}>
-                    設定
-                    <Link to='/settings' />
-                  </MenuItem>
-                </Menu>
-              </SidebarContent>
-            </ProSidebar>
+    <Authenticator variation='modal'>
+      {({signOut, user }) => (
+        <div className='App'>
+          <ProSidebar>
+            <SidebarHeader>
+              <div
+                style={{
+                  padding: '24px',
+                  textTransform: 'uppercase',
+                  fontWeight: 'bold',
+                  fontSize: 14,
+                  letterSpacing: '1px',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'nowrap',
+                }}
+              >
+                Attendance App
+              </div>
+            </SidebarHeader>
+            <SidebarContent>
+              <Menu iconShape='square'>
+                <SubMenu title='マイページ' icon={<FaHeart />}>
+                  <MenuItem>打刻</MenuItem>
+                  <MenuItem>出勤簿</MenuItem>
+                  <MenuItem>申請</MenuItem>
+                  <MenuItem>メンバー</MenuItem>
+                  <MenuItem>所属部署</MenuItem>
+                </SubMenu>
+                <MenuItem icon={<FaUserCheck />} style={getStyle('application')} id='application'>
+                  承認
+                  <Link to='/applications' />
+                </MenuItem>
+                <MenuItem icon={<MdGroup />} style={getStyle('groups')} id='groups'>
+                  部署
+                  <Link to='/groups' />
+                </MenuItem>
+                <MenuItem icon={<FaUser />} style={getStyle('users')} id='users'>
+                  社員
+                  <Link to='/users' />
+                </MenuItem>
+              </Menu>
+              <Menu>
+                <MenuItem icon={<FaRegSun />}>
+                  設定
+                  <Link to='/settings' />
+                </MenuItem>
+              </Menu>
+            </SidebarContent>
+          </ProSidebar>
 
-            <main>
-              <Header signOut={signOut} />
-              <Routes>
-                <Route path='/mypage'>
-                </Route>
-                <Route path='/groups' element={<GroupTable />}>
-                </Route>
-                <Route path='/groups/:id' element={<GroupInfo />}>
-                </Route>
-                <Route path='/users' element={<UserTable />}>
-                </Route>
-                <Route path='/users/:id' element={<UserInfo />}>
-                </Route>
-                <Route path='/settings'>
-                </Route>
-              </Routes>
-            </main>
-          </div>
-        )}
-      </Authenticator>
-    </div>
+          <main>
+            <Header signOut={signOut} />
+            <Routes>
+              <Route path='/mypage'>
+              </Route>
+              <Route path='/groups' element={<GroupTable />}>
+              </Route>
+              <Route path='/groups/:id' element={<GroupInfo />}>
+              </Route>
+              <Route path='/users' element={<UserTable />}>
+              </Route>
+              <Route path='/users/:id' element={<UserInfo />}>
+              </Route>
+              <Route path='/settings'>
+              </Route>
+            </Routes>
+          </main>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
